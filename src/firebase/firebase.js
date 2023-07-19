c21939d32dde62b3fcb5b76d378284bb689f2398
AkhilPanwar <panwarakhil1811@gmail.com>
2023-07-19 17:24:05 +0530

    hide-credentials

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 
 
   const firebaseConfig = {
-    apiKey: "AIzaSyBFsQ1rEw7k3yvtAejODOgkzFlZiAOk3bM",
-    authDomain: "healthcare-349da.firebaseapp.com",
-    projectId: "healthcare-349da",
-    storageBucket: "healthcare-349da.appspot.com",
-    messagingSenderId: "730513019025",
-    appId: "1:730513019025:web:0a938a3e4f27639c37af30",
-    measurementId: "G-LGFTRFVCJ7"
+    apiKey:process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket:process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGE,
+    appId: Process.env.FIREBASE_APP_ID,
+    measurementId:process.env.FIREBASE_MEASUREMENT_ID
   };
 
 // Initialize Firebase
